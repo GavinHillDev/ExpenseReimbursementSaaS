b635ca60385588352d0fdffe98fe0802e1575968
Gavin <88601252+GavinHillDev@users.noreply.github.com>
2026-01-08 22:50:22 -0500

    Initial commit

---
+++ ExpenseReimbursmentSaaS/Models/Receipt.cs
@@ -0,0 +1,17 @@
+﻿namespace ExpenseReimbursmentSaaS.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+
+        public int ExpentReportId { get; set; }
+
+        public ExpenseReport expenseReport { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string ContentType { get; set; }
+
+        public DateOnly UploadeDate { get; set; }
+    }
+}
