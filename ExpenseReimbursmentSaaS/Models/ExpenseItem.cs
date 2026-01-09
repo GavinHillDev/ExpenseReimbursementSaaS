f5c2495589cff69a57191ca99f82cdc271fd8bf6
Gavin <88601252+GavinHillDev@users.noreply.github.com>
2026-01-08 23:02:18 -0500

    Initial commit

---
+++ ExpenseReimbursmentSaaS/Models/ExpenseItem.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpenseReimbursmentSaaS.Models
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+
+        public int ExpenseReportId { get; set; }
+
+        public ExpenseReport expenseReport { get; set; }
+
+        public double Amount { get; set; }  
+
+        public string Category { get; set; } 
+
+        public DateOnly Date { get; set; }
+
+        public string Description { get; set; } 
+    }
+}
---
+++ ExpenseReimbursmentSaaS/Models/ExpenseReport.cs
@@ -0,0 +1,20 @@
+﻿namespace ExpenseReimbursmentSaaS.Models
+{
+    public class ExpenseReport
+    {
+        public int Id { get; set; }
+
+        public DateOnly UploadDate { get; set;}
+
+        public string Status { get; set; }
+
+        public int totalAmount { get; set; }
+
+        public string? managerComment { get; set; }
+
+        public string? financeCommnet { get; set; }
+
+
+
+    }
+}
